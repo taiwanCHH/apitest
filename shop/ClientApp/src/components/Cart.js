d930b8a949f621f14a115d826d640e8524481dc5
steve <photohtury@gmail.com>
2021-05-05 10:28:43 +0800

    add member menu.

--- shop/ClientApp/src/components/Cart.js
+++ shop/ClientApp/src/components/Cart.js
@@ -1,66 +1,66 @@
 import React, { Component } from 'react';
-import {ContextStore} from '../index.js';
+import { ContextStore } from '../index.js';
 import { Button, Table, Alert } from 'reactstrap';
 
-export function Cart(){
+export function Cart() {
     const value = React.useContext(ContextStore)
-  
+
     return (
         <React.Fragment>
-          {
-            value.cart.length
-          }
+            {
+                value.cart.length
+            }
         </React.Fragment>
-      )
+    )
 }
 
-export function CartCount(){
+export function CartCount() {
     const value = React.useContext(ContextStore)
-  
+
     return (
         <React.Fragment>
-          {
-            value.cart.length
-          }
+            {
+                value.cart.length
+            }
         </React.Fragment>
-      )
+    )
 }
 
-export function CartTable(){
+export function CartTable() {
     const { cart, dispatch } = React.useContext(ContextStore);
     const deleteCartItem = (index) => {
         // const cart = this.state.cart;
         // cart.splice(index, 1);
-    
+
         // this.setState({
         //   cart
         // });
-      }
-    return(
+    }
+    return (
         <React.Fragment>
             <h1>Cart</h1>
-          <Table>
+            <Table>
                 <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>品項</th>
-                    <th>價格</th>
-                    <th />
-                  </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>品項</th>
+                        <th>價格</th>
+                        <th />
+                    </tr>
                 </thead>
                 <tbody>
-                  {
-                    cart.map((item, index) => (
-                      <tr>
-                        <th scope="row">{index + 1}</th>
-                        <td width="250">{item.name}</td>
-                        <td>{item.price}</td>
-                        <td><Button color="danger" onClick={() => deleteCartItem(index)}>X</Button>{' '}</td>
-                      </tr>
-                    ))
-                  }
+                    {
+                        cart.map((item, index) => (
+                            <tr key={index} >
+                                <th scope="row">{index + 1}</th>
+                                <td width="250">{item.product.name}</td>
+                                <td>{item.product.price}</td>
+                                <td><Button color="danger" onClick={() => deleteCartItem(index)}>X</Button>{' '}</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
-              </Table>
+            </Table>
         </React.Fragment>
     )
 }
--- shop/ClientApp/src/components/NavMenu.js
+++ shop/ClientApp/src/components/NavMenu.js
@@ -1,49 +1,53 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import React, { Component, useState } from 'react';
+import {
+  Collapse, Container, Navbar, NavbarBrand, NavbarToggler,
+  NavItem, NavLink, Dropdown, DropdownItem, DropdownToggle, DropdownMenu
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import {CartCount} from './Cart.js';
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+import { CartCount } from './Cart.js';
 
-  constructor (props) {
-    super(props);
+export const NavMenu = () => {
+  const [collapsed, setcollapsed] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isAuth, setIsAuth] = useState(true);
+  const [title, setTitle] = useState('Member');
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+  const toggleNavbar = () => setcollapsed(!collapsed);
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
 
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">shop</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/cart">Cart(<CartCount/>)</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Container>
+          <NavbarBrand tag={Link} to="/">shop</NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/cart">Cart(<CartCount />)</NavLink>
+              </NavItem>
+              <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle nav caret>
+                  {title}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem tag="a" href="/cart" style={{ display: isAuth ? "none" : "block" }}>Sign in</DropdownItem>
+                  <DropdownItem tag="a" href="/cart" style={{ display: isAuth ? "none" : "block" }}>Sign up</DropdownItem>
+                  <DropdownItem tag="a" href="/cart" style={{ display: isAuth ? "block" : "none" }}>Info</DropdownItem>
+                  <DropdownItem tag="a" href="/cart" style={{ display: isAuth ? "block" : "none" }}>Change Password</DropdownItem>
+                  <DropdownItem style={{ display: isAuth ? "block" : "none" }}>Log Out</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </ul>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+
 }
