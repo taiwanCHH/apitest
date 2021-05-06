86f90f2aed316babb0099163e88f94fe94327562
steve <photohtury@gmail.com>
2021-05-06 10:48:06 +0800

    add register

--- shop/ClientApp/src/components/Member.js
+++ shop/ClientApp/src/components/Member.js
@@ -1,70 +1,115 @@
-import React, { Component,useState } from 'react';
+import React, { Component, useState } from 'react';
 import { ContextStore } from '../index.js';
 import { Button, Form, FormGroup, Label, Input, FormFeedback, FormText } from 'reactstrap';
-import {checkPassWordValidity, checkValidity} from "../shared/utility";
+import { checkPassWordValidity, checkEmptyValidity, checkEmailValidity } from "../shared/utility";
+import axios from 'axios';
 
 export const Member = (props) => {
     const { cart, dispatch } = React.useContext(ContextStore);
 
-    const [password, setPassword] = useState({
+    const [info, setInfo] = useState({
+        name: "",
+        email: "",
         firstPassword: "",
         secondPassword: "",
-       });
+        sex: "M",
+        phone: "",
+        birthday: "",
+    });
+    const [errorName, setErrorName] = useState('');
+    const [errorEmail, setErrorEmail] = useState('');
     const [errorPassword, setErrorPassword] = useState('');
-  
-  
-  const setFirst = (event) => {
-    setPassword({ ...password, firstPassword: event.target.value });
-  };
-  const setSecond = (event) => {
-    setPassword({ ...password, secondPassword: event.target.value });
-  };
-  
-  const submit=()=>{
-      var errorMessage=checkPassWordValidity(password.firstPassword,password.secondPassword)
-      console.log('v:'+errorMessage)
-      setErrorPassword(errorMessage)
-      console.log(checkValidity(password.firstPassword,{required:true}))
-  }
+    const [errorSex, setErrorSex] = useState('');
+    const [errorPhone, setErrorPhone] = useState('');
+    const [errorBirthday, setErrorBirthday] = useState('');
+
+    const setInfoName = (event) => { setInfo({ ...info, name: event.target.value }); };
+    const setInfoEmail = (event) => { setInfo({ ...info, email: event.target.value }); };
+    const setInfoFirst = (event) => { setInfo({ ...info, firstPassword: event.target.value }); };
+    const setInfoSecond = (event) => { setInfo({ ...info, secondPassword: event.target.value }); };
+    const setInfoSex = (event) => { setInfo({ ...info, sex: event.target.value }); };
+    const setInfoPhone = (event) => { setInfo({ ...info, phone: event.target.value }); };
+    const setInfoBirthday = (event) => { setInfo({ ...info, birthday: event.target.value }); };
+
+    const submit = () => {
+        setErrorName(checkEmptyValidity(info.name))
+        setErrorEmail(checkEmailValidity(info.email))
+        setErrorPassword(checkPassWordValidity(info.firstPassword, info.secondPassword))
+        setErrorSex(checkEmptyValidity(info.sex))
+        setErrorPhone(checkEmptyValidity(info.phone))
+        setErrorBirthday(checkEmptyValidity(info.birthday))
+
+        if (errorName.length === 0 &&
+            errorEmail.length === 0 &&
+            errorPassword.length === 0 &&
+            errorSex.length === 0 &&
+            errorPhone.length === 0 &&
+            errorBirthday.length === 0) {
+            const user = {
+                "userPhone": info.phone,
+                "userSex": info.sex,
+                "userBirthday": info.birthday,
+                "userName": info.name,
+                "email": info.email,
+                "password": info.firstPassword
+
+            };
+            axios.post('/api/AuthManagement/Register', user)
+                .then(response => console.log(response.data))
+                .catch((error) => {console.log('There was an error!', error.data.errors);});
+                
+        }
+
+    }
 
     return (
         <Form>
-          <FormGroup>
-            <Label for="name">暱稱</Label>
-            <Input type="email" name="name" id="name" placeholder="暱稱" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="email">Email</Label>
-            <Input type="email" name="email" id="email" placeholder="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">密碼</Label>
-            <Input invalid={ errorPassword.length>0 }  type="password" onChange={setFirst} name="password" id="password" placeholder="密碼" />
-            <FormFeedback >{errorPassword}</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label for="password2">確認密碼</Label>
-            <Input type="password" onChange={setSecond} name="password2" id="password2" placeholder="確認密碼" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="sex">性別</Label>
-            <Input type="select" name="sex" id="sex">
-              <option>男</option>
-              <option>女</option>
-            </Input>
-          </FormGroup>
-          <FormGroup>
-        <Label for="birthday">生日</Label>
-        <Input
-          type="date"
-          name="birthday"
-          id="birthday"
-          placeholder="生日"
-        />
-      </FormGroup>
-          <Button onClick={() => submit()}>Submit</Button>
+            <FormGroup>
+                <Label for="name" >暱稱</Label>
+                <Input invalid={errorName.length > 0} type="text" onChange={setInfoName} name="name" id="name" placeholder="暱稱" />
+                <FormFeedback >{errorName}</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+                <Label for="email">Email</Label>
+                <Input invalid={errorEmail.length > 0} type="email" onChange={setInfoEmail} name="email" id="email" placeholder="email" />
+                <FormFeedback >{errorEmail}</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+                <Label for="password">密碼</Label>
+                <Input invalid={errorPassword.length > 0} type="password" onChange={setInfoFirst} name="password" id="password" placeholder="密碼" />
+                <FormFeedback >{errorPassword}</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+                <Label for="password2">確認密碼</Label>
+                <Input type="password" onChange={setInfoSecond} name="password2" id="password2" placeholder="確認密碼" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="sex">性別</Label>
+                <Input invalid={errorSex.length > 0} value={info.sex} onChange={setInfoSex} type="select" name="sex" id="sex">
+                    <option value='M'>男</option>
+                    <option value='F'>女</option>
+                </Input>
+                <FormFeedback >{errorSex}</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+                <Label for="phone">電話</Label>
+                <Input invalid={errorPhone.length > 0} type="number" onChange={setInfoPhone} name="phone" id="phone" placeholder="09xx 或 02xx" />
+                <FormFeedback >{errorPhone}</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+                <Label for="birthday">生日</Label>
+                <Input
+                    invalid={errorBirthday.length > 0}
+                    type="date"
+                    name="birthday"
+                    id="birthday"
+                    onChange={setInfoBirthday}
+                />
+                <FormFeedback >{errorBirthday}</FormFeedback>
+            </FormGroup>
+            <Button className="btn btn-primary" onClick={() => submit()}>Submit</Button>
         </Form>
-      );
+    );
 
 }
 
--- shop/ClientApp/src/shared/utility.js
+++ shop/ClientApp/src/shared/utility.js
@@ -4,51 +4,65 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     };
 };
-export const checkPassWordValidity = ( firstPassword,secondPassword ) => {
-    let error=''
-    let requiredLength=6
-    
-    if(error.length===0){
-        let len=(firstPassword.length >= requiredLength ? true : false);
-        error=len?'':'長度不夠,要有六位密碼'
+export const checkPassWordValidity = (firstPassword, secondPassword) => {
+    let error = ''
+    let requiredLength = 6
+    if (error.length === 0) {
+        let len = (firstPassword.length >= requiredLength ? true : false);
+        error = len ? '' : '長度不夠,至少要有六位密碼'
     }
-    if(error.length===0){   
-        let num=(/\d/.test(firstPassword));
-        error=num?"":"至少包含一個數字"
+    if (error.length === 0) {
+        let num = (/\d/.test(firstPassword));
+        error = num ? "" : "至少包含一個數字"
     }
-    if(error.length===0){
-        let mat=(firstPassword && firstPassword === secondPassword);
-        error=mat?'':'確認密碼輸入不符'
+    if (error.length === 0) {
+        let mat = (firstPassword && firstPassword === secondPassword);
+        error = mat ? '' : '確認密碼輸入不符'
     }
     return error
 }
+export const checkEmptyValidity = (value) => {
+    let error = ''
+    if (error.length === 0) {
+        let isValid = (value.trim().length > 0);
+        error = isValid ? "" : "輸入不能空白"
+    }
+    return error
+}
+export const checkEmailValidity =(value)=>{
+    let error = ''
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    let isValid = pattern.test(value)
+    error = isValid ? "" : "請輸入Email格式"
+    return error
+}
 
-export const checkValidity = ( value, rules ) => {
+export const checkValidity = (value, rules) => {
     let isValid = true;
-    if ( !rules ) {
+    if (!rules) {
         return true;
     }
 
-    if ( rules.required ) {
+    if (rules.required) {
         isValid = value.trim() !== '' && isValid;
     }
 
-    if ( rules.minLength ) {
+    if (rules.minLength) {
         isValid = value.length >= rules.minLength && isValid
     }
 
-    if ( rules.maxLength ) {
+    if (rules.maxLength) {
         isValid = value.length <= rules.maxLength && isValid
     }
 
-    if ( rules.isEmail ) {
+    if (rules.isEmail) {
         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test( value ) && isValid
+        isValid = pattern.test(value) && isValid
     }
 
-    if ( rules.isNumeric ) {
+    if (rules.isNumeric) {
         const pattern = /^\d+$/;
-        isValid = pattern.test( value ) && isValid
+        isValid = pattern.test(value) && isValid
     }
 
     return isValid;
